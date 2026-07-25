de8569ae9f5d9df48aaf5cb9678810b93258f987
kirubasankari02 <kirubashankariram@gmail.com>
2026-07-25 01:16:07 -0400

    Configure azure remote backend

--- providers.tf
+++ providers.tf
@@ -5,6 +5,15 @@ terraform {
       version = "4.81.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "sttfstateks5339"
+    container_name       = "tfstate"
+    key                  = "terraform-azure-storage.tfstate"
+    use_cli = true
+    use_azuread_auth = true
+  }
 }
 
 provider "azurerm" {
